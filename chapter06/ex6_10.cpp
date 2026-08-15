ed850988a882f664ef59b2258d29b5387db53afb
voyers <2240710889@qq.com>
2026-08-15 16:34:21 +0800

    完成练习6.10（指针交换）

---
+++ chapter06/ex6_10.cpp
@@ -0,0 +1,26 @@
+# include <iostream>
+# include <string>
+# include <vector>
+using std::cin;using std::cout;using std::endl;using std::string;
+
+int covert(int *p,int *q)
+{
+    
+    int temp;
+    temp = *p;
+    *p = *q;
+    *q = temp;
+    return 0;
+}
+
+int main()
+{
+    int i,j;
+    string s = "输入两个整数";
+    cout << s << endl;
+    cin >> i >> j;
+    covert(&i,&j);
+    cout << "交换后结果为：" << endl;
+    cout << i << " " << j <<endl;
+    return 0;
+}
