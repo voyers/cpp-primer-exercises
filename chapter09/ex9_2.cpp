0cbb91d5b43f8942111eb8a2f7cd7981df765043
voyers <2240710889@qq.com>
2026-08-23 22:51:00 +0800

    完成练习9.2：容器嵌套

---
+++ chapter09/ex9_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <deque>
+#include <list>
+#include <algorithm>
+#include <fstream>
+using std::string;using std::vector;using std::cin;using std::cout;
+using std::endl;using std::deque;using std::list;
+int main()
+{
+    list <deque<int>> num;
+    return 0;
+}
